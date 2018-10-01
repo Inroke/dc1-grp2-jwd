75699ad146926defc1a3cf32ef2170a61924c034
Adrien Tanferri <adrientanferri@MacBook-Pro-de-Adrien.local>
2018-10-01 16:31:27 +0200

    Ajout de la suppression et gestion des messages d'erreur si on supprime une catégorie ou il y a des images

--- admin/crud/categorie/index.php
+++ admin/crud/categorie/index.php
@@ -1,5 +1,19 @@
 <?php require_once '../../../model/database.php';
 $liste_categories = getAllEntities("categorie");
+
+$error_msg = null;
+
+if (isset($_GET['errcode'])) {
+    switch ($_GET["errcode"]) {
+        case 23000:
+            $error_msg = "Zut, erreur lors de la suppression !";
+            break;
+        default:
+            $error_msg = "une erreur est survenue !";
+            break;
+    }
+}
+
 require_once '../../layout/header.php'?>
 
 
@@ -10,11 +24,18 @@ require_once '../../layout/header.php'?>
     Ajouter
 </a>
 
-<table class="table table-striped taple-bordered">
+<?php if ($error_msg) : ?>
+<div class="alert alert-danger">
+    <i class="fa fa-times"></i>
+    <?php echo $error_msg; ?>
+</div>
+<?php endif ?>
+
+<table class="table table-striped table-bordered">
     <thead>
         <tr>
             <th>Titre</th>
-            <th>Actions</th>
+            <th class="actions">Actions</th>
         </tr>
     </thead>
     <tbody>
@@ -24,10 +45,19 @@ require_once '../../layout/header.php'?>
                 <?php echo $categorie['libelle']; ?>
             </td>
             <td>
-                <a href="update.php?id=<?php echo $categorie["id"]; ?>" class="btn btn-warning">
-                    <i class="fa fa-edit"></i>
-                    Modifier
-                </a>
+                <div class="actions">
+                    <a href="update.php?id=<?php echo $categorie["id"]; ?>" class="btn btn-warning">
+                        <i class="fa fa-edit"></i>
+                        Modifier
+                    </a>
+                    <form action="delete_query.php" method="POST">
+                        <input type="hidden" name="id" value="<?php echo $categorie["id"];?>">
+                        <button class="btn btn-danger">
+                            <i class="fa fa-trash"></i>
+                            Supprimer
+                        </button>
+                    </form>
+                </div>
             </td>
         </tr>
         <?php endforeach;?>
